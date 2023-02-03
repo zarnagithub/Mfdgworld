480974b83cee45d697886dbd565ae56da6b9aec0
makorp2 <zarna.gohil1974@gmail.com>
2023-02-03 18:13:39 +0530

    java activity files updated.

--- app/src/main/java/com/example/mfdgworld/MainActivity.java
+++ app/src/main/java/com/example/mfdgworld/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.electricity_sub_category);
         Intent intent= new Intent(MainActivity.this,LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
